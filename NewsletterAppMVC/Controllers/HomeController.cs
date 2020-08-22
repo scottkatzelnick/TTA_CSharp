1562315b1b73241da436e1d4d608e588354c39c1
Scott Katzelnick <scott@devsmk.com>
2020-08-22 00:35:57 -0400

    Update NewsletterApp w/ Entity Framework

--- NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/Controllers/HomeController.cs
@@ -1,19 +1,10 @@
 ﻿using NewsletterAppMVC.Models;
-using NewsletterAppMVC.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Web.Mvc;
 
 namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;
-                                            Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;
-                                            MultiSubnetFailover=False";
-
         public ActionResult Index()
         {
             return View();
@@ -28,67 +19,18 @@ namespace NewsletterAppMVC.Controllers
             }
             else
             {
-                string queryString = @"INSERT INTO SignUps (FirstName, LastName, Email)
-                                        VALUES (@FirstName, @LastName, @Email)";
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (NewsletterEntities db = new NewsletterEntities())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@Email", SqlDbType.VarChar);
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.Email = email;
 
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@Email"].Value = email;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
                 }
                 return View("Success");
             }
-        }
-
-        public ActionResult Admin()
-        {
-            using (NewsletterEntities db = new NewsletterEntities())
-            {
-                var signups = db.SignUps;
-                var SignUpVms = new List<SignUpVm>();
-                foreach (var signup in signups)
-                {
-                    var signupVm = new SignUpVm();
-                    signupVm.FirstName = signup.FirstName;
-                    signupVm.LastName = signup.LastName;
-                    signupVm.Email = signup.Email;
-
-                    SignUpVms.Add(signupVm);
-                }
-                return View(SignUpVms);
-            }
-            //string queryString = @"SELECT * FROM SignUps";
-
-            //List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //    SqlCommand command = new SqlCommand(queryString, connection);
-            //    connection.Open();
-            //    SqlDataReader reader = command.ExecuteReader();
-
-            //    while (reader.Read())
-            //    {
-            //        var signup = new NewsletterSignUp();
-            //        signup.Id = Convert.ToInt32(reader["Id"]);
-            //        signup.FirstName = reader["FirstName"].ToString();
-            //        signup.LastName = reader["LastName"].ToString();
-            //        signup.Email = reader["Email"].ToString();
-            //        signup.SocialSecurityNumber = reader["SocialSecurityNumber"].ToString();
-
-            //        signups.Add(signup);
-            //    }
-            //}
-
         }
     }
 }
